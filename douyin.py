291ce6c5e24197b0621a9a569a14f003defd6c9e
Wade <wadechen@outlook.com>
2023-08-25 23:48:08 +0800

    modify douyin class

--- douyin.py
+++ douyin.py
@@ -11,120 +11,142 @@ import websocket
 from protobuf import dy_pb2
 
 
-def get_room_info(url):
-    payload = {}
-    headers = {
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
-                  'application/signed-exchange;v=b3;q=0.9',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/107.0.0.0 Safari/537.36',
-        'cookie': '__ac_nonce=0638733a400869171be51',
-    }
+class Douyin:
 
-    proxies = dict(http="", https="")
+    def __init__(self, url):
+        self.ws_conn = None
+        self.url = url
 
-    response = requests.get(url, headers=headers, data=payload, proxies=proxies)
-    res_cookies = response.cookies
-    ttwid = res_cookies.get_dict().get("ttwid")
-    res_origin_text = response.text
-    re_pattern = r'<script id="RENDER_DATA" type="application/json">(.*?)</script>'
-    re_obj = re.compile(re_pattern)
-    match = re_obj.search(res_origin_text)
-    if match:
-        match_text = match.group(1)
-        res_text = urllib.parse.unquote(match_text)
-        app_data = json.loads(res_text)
-        app_json = app_data.get("app")
-        initial_state_json = app_json.get("initialState")
-        room_store_json = initial_state_json.get("roomStore")
-        live_room_id = room_store_json.get("roomInfo").get("roomId")
-        live_room_title = room_store_json.get("roomInfo").get("room").get("title")
-        return {
-            'url': url,
-            'ttwid': ttwid,
-            'room_store': room_store_json,
-            'room_id': live_room_id,
-            'room_title': live_room_title,
+    def _get_room_info(self):
+        payload = {}
+        headers = {
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,'
+                      '*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/107.0.0.0 Safari/537.36',
+            'cookie': '__ac_nonce=0638733a400869171be51',
         }
-    else:
-        return None
 
+        proxies = dict(http="", https="")
 
-def connect_web_socket(room_info):
-    ws_url = (r'wss://webcast3-ws-web-lq.douyin.com/webcast/im/push/v2/?app_name=douyin_web&version_code=180800'
-              r'&webcast_sdk_version=1.3.0&update_version_code=1.3.0&compress=gzip&internal_ext=internal_src:dim'
-              r'|wss_push_room_id:%s|wss_push_did:%s|dim_log_id:202302171547011A160A7BAA76660E13ED|fetch_time'
-              r':1676620021641|seq:1|wss_info:0-1676620021641-0-0|wrds_kvs:WebcastRoomStatsMessage'
-              r'-1676620020691146024_WebcastRoomRankMessage-1676619972726895075_AudienceGiftSyncData'
-              r'-1676619980834317696_HighlightContainerSyncData-2&cursor=t-1676620021641_r-1_d-1_u-1_h-1&host=https'
-              r'://live.douyin.com&aid=6383&live_id=1&did_rule=3&debug=false&endpoint=live_pc&support_wrds=1&im_path'
-              r'=/webcast/im/fetch/&user_unique_id=%s&device_platform=web&cookie_enabled=true&screen_width=1440'
-              r'&screen_height=900&browser_language=zh&browser_platform=MacIntel&browser_name=Mozilla&browser_version'
-              r'=5.0%20(Macintosh;%20Intel%20Mac%20OS%20X%2010_15_7)%20AppleWebKit/537.36%20(KHTML,'
-              r'%20like%20Gecko)%20Chrome/110.0.0.0%20Safari/537.36&browser_online=true&tz_name=Asia/Shanghai'
-              r'&identity=audience&room_id=%s&heartbeatDuration=0&signature=00000000')
+        response = requests.get(self.url, headers=headers, data=payload, proxies=proxies)
+        res_cookies = response.cookies
+        ttwid = res_cookies.get_dict().get("ttwid")
+        res_origin_text = response.text
+        re_pattern = r'<script id="RENDER_DATA" type="application/json">(.*?)</script>'
+        re_obj = re.compile(re_pattern)
+        match = re_obj.search(res_origin_text)
+        if match:
+            try:
+                match_text = match.group(1)
+                res_text = urllib.parse.unquote(match_text)
+                app_data = json.loads(res_text)
+                app_json = app_data.get("app")
+                initial_state_json = app_json.get("initialState")
+                room_store_json = initial_state_json.get("roomStore")
+                live_room_id = room_store_json.get("roomInfo").get("roomId")
+                live_room_title = room_store_json.get("roomInfo").get("room").get("title")
+                self.room_info = {
+                    'url': self.url,
+                    'ttwid': ttwid,
+                    'room_store': room_store_json,
+                    'room_id': live_room_id,
+                    'room_title': live_room_title,
+                }
+            except Exception as e:
+                self.room_info = None
+        else:
+            self.room_info = None
 
-    ws_url = ws_url.replace("%s", room_info.get('room_id'))
-    headers = {
-        'cookie': 'ttwid=' + room_info.get('ttwid'),
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
-                      'Chrome/108.0.0.0 Safari/537.36'
-    }
+    def connect_web_socket(self):
+        self._get_room_info()
+        if self.room_info is None:
+            logging.error(f"获取直播间({self.url})信息失败")
+            return
+        ws_url = (r'wss://webcast3-ws-web-lq.douyin.com/webcast/im/push/v2/?app_name=douyin_web&version_code=180800'
+                  r'&webcast_sdk_version=1.3.0&update_version_code=1.3.0&compress=gzip&internal_ext=internal_src:dim'
+                  r'|wss_push_room_id:%s|wss_push_did:%s|dim_log_id:202302171547011A160A7BAA76660E13ED|fetch_time'
+                  r':1676620021641|seq:1|wss_info:0-1676620021641-0-0|wrds_kvs:WebcastRoomStatsMessage'
+                  r'-1676620020691146024_WebcastRoomRankMessage-1676619972726895075_AudienceGiftSyncData'
+                  r'-1676619980834317696_HighlightContainerSyncData-2&cursor=t-1676620021641_r-1_d-1_u-1_h-1&host'
+                  r'=https://live.douyin.com&aid=6383&live_id=1&did_rule=3&debug=false&endpoint=live_pc&support_wrds'
+                  r'=1&im_path=/webcast/im/fetch/&user_unique_id=%s&device_platform=web&cookie_enabled=true'
+                  r'&screen_width=1440&screen_height=900&browser_language=zh&browser_platform=MacIntel&browser_name'
+                  r'=Mozilla&browser_version=5.0%20('
+                  r'Macintosh;%20Intel%20Mac%20OS%20X%2010_15_7)%20AppleWebKit/537.36%20(KHTML,'
+                  r'%20like%20Gecko)%20Chrome/110.0.0.0%20Safari/537.36&browser_online=true&tz_name=Asia/Shanghai'
+                  r'&identity=audience&room_id=%s&heartbeatDuration=0&signature=00000000')
 
-    websocket.enableTrace(False)
-    ws_conn = websocket.WebSocketApp(ws_url,
-                                     header=headers,
-                                     on_message=on_message,
-                                     on_open=on_open,
-                                     on_error=on_error,
-                                     on_close=on_close)
+        ws_url = ws_url.replace("%s", self.room_info.get('room_id'))
+        headers = {
+            'cookie': 'ttwid=' + self.room_info.get('ttwid'),
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/108.0.0.0 Safari/537.36'
+        }
 
-    ws_conn.run_forever(reconnect=1)
+        websocket.enableTrace(False)
+        self.ws_conn = websocket.WebSocketApp(ws_url,
+                                              header=headers,
+                                              on_message=self._on_message,
+                                              on_open=self._on_open,
+                                              on_error=self._on_error,
+                                              on_close=self._on_close)
 
+        self.ws_conn.run_forever(reconnect=1)
 
-def on_message(ws, message):
-    msg_pack = dy_pb2.PushFrame()
-    msg_pack.ParseFromString(message)
-    decompressed_payload = gzip.decompress(msg_pack.payload)
-    payload_package = dy_pb2.Response()
-    payload_package.ParseFromString(decompressed_payload)
-    for msg in payload_package.messagesList:
-        match msg.method:
-            case 'WebcastChatMessage':
-                parse_chat_msg(msg.payload)
-                break
-            case "WebcastGiftMessage":
-                break
-            case "WebcastLikeMessage":
-                break
-            case "WebcastMemberMessage":
-                break
-            case 'WebcastInRoomBannerMessage':
-                break
-            case 'WebcastRoomRankMessage':
-                break
-            case 'WebcastRoomDataSyncMessage':
-                break
-            case _:
-                break
+    def _send_ask(self, log_id, internal_ext):
+        ack_pack = dy_pb2.PushFrame()
+        ack_pack.logId = log_id
+        ack_pack.payloadType = internal_ext
 
+        data = ack_pack.SerializeToString()
+        self.ws_conn.send(data, opcode=websocket.ABNF.OPCODE_BINARY)
 
-def on_error(ws, error):
-    logging.error(error)
+    def _on_message(self, ws, message):
+        msg_pack = dy_pb2.PushFrame()
+        msg_pack.ParseFromString(message)
+        decompressed_payload = gzip.decompress(msg_pack.payload)
+        payload_package = dy_pb2.Response()
+        payload_package.ParseFromString(decompressed_payload)
+        if payload_package.needAck:
+            self._send_ask(msg_pack.logId, payload_package.internalExt)
+        for msg in payload_package.messagesList:
+            match msg.method:
+                case 'WebcastChatMessage':
+                    self._parse_chat_msg(msg.payload)
+                    break
+                case "WebcastGiftMessage":
+                    break
+                case "WebcastLikeMessage":
+                    break
+                case "WebcastMemberMessage":
+                    break
+                case 'WebcastInRoomBannerMessage':
+                    break
+                case 'WebcastRoomRankMessage':
+                    break
+                case 'WebcastRoomDataSyncMessage':
+                    break
+                case _:
+                    break
 
+    @staticmethod
+    def _on_error(ws, error):
+        logging.error(error)
 
-def on_close(ws, _, __):
-    logging.info("Websocket closed")
+    @staticmethod
+    def _on_close(ws, close_status_code, close_msg):
+        logging.info("Websocket closed")
 
+    @staticmethod
+    def _on_open(ws):
+        logging.info("Websocket opened")
 
-def on_open(ws):
-    logging.info("Websocket opened")
-
-
-def parse_chat_msg(payload):
-    payload_pack = dy_pb2.ChatMessage()
-    payload_pack.ParseFromString(payload)
-    formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(payload_pack.eventTime))
-    user_name = payload_pack.user.nickName
-    content = payload_pack.content
-    logging.info(f"{formatted_time} [弹幕] {user_name}: {content}")
+    @staticmethod
+    def _parse_chat_msg(payload):
+        payload_pack = dy_pb2.ChatMessage()
+        payload_pack.ParseFromString(payload)
+        formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(payload_pack.eventTime))
+        user_name = payload_pack.user.nickName
+        content = payload_pack.content
+        print(f"{formatted_time} [弹幕] {user_name}: {content}")
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import logging
 
 import config
-import douyin
+from douyin import Douyin
 
 if __name__ == '__main__':
     url = config.content['url']
     logging.basicConfig(level=config.content['log']['level'], format=config.content['log']['format'])
-    room_info = douyin.get_room_info(url)
-    if room_info is not None:
-        douyin.connect_web_socket(room_info)
+    dy = Douyin(url)
+    dy.connect_web_socket()
